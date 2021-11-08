2359e472273c69ece37198e706d85ffa3a78871e
Shkaff02 <ksurygin5@gmail.com>
2021-11-08 14:44:02 +0200

    "Remove duplicates"

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Remove duplicates");
+        int[] arr = {1,2,2};
+        System.out.println("Result " + ArrayTasks.removeDuplicates(arr));
+    }
+}
