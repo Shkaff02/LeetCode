8efe2e415be7bbb06ba45f2e92b4015519b4f322
Shkaff02 <ksurygin5@gmail.com>
2021-11-08 15:25:26 +0200

    "Best Time to Buy and Sell Stock II"

--- src/ArrayTasks.java
+++ src/ArrayTasks.java
@@ -1,5 +1,6 @@
 public class ArrayTasks {
     /*
+    Remove duplicates
     Given an integer array nums sorted in non-decreasing order,
     remove the duplicates in-place such that each unique element
     appears only once. The relative order of the elements should
@@ -26,5 +27,29 @@ public class ArrayTasks {
             }
         }
         return i + 1;
+    }
+
+    /*
+      Best Time to Buy and Sell Stock II
+
+    You are given an integer array prices where prices[i] is the price
+    of a given stock on the ith day.
+
+    On each day, you may decide to buy and/or sell the stock.
+    You can only hold at most one share of the stock at any time.
+    However, you can buy it then immediately sell it on the same
+    day.
+
+    Find and return the maximum profit you can achieve.
+     */
+
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i+1]) {
+                profit += prices[i+1] - prices[i];
+            }
+        }
+        return profit;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Remove duplicates");
         int[] arr = {1,2,2};
         System.out.println("Result " + ArrayTasks.removeDuplicates(arr));
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Best Time to Buy and Sell Stock II");
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println("Result " + ArrayTasks.maxProfit(prices));
     }
 }
