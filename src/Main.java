fa51ff3c50386492f96c1e6bdaa6d6514526293d
Shkaff02 <ksurygin5@gmail.com>
2021-11-08 15:46:45 +0200

    "Reverse string"

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Reverse string");
+        char[] str = {'h', 'e', 'l', 'l', 'o'};
+        StringTasks.reverseString(str);
+        System.out.println();
+        System.out.println("----------------------------------");
+    }
+}
---
+++ src/StringTasks.java
@@ -0,0 +1,20 @@
+public class StringTasks {
+    /*
+        Reverse string
+
+        Write a function that reverses a string. The input string is given as an array of characters s.
+     */
+
+    public static void reverseString(char[] s) {
+        char temp;
+        for(int i = 0; i < s.length/2; i++) {
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i]);
+        }
+    }
+
+}
