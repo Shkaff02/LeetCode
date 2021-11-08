d85a1837b8b4765f38253cd4f1479fc792fc2ee4
Shkaff02 <ksurygin5@gmail.com>
2021-11-08 15:56:41 +0200

    "First unique character in a string"

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("Reverse integer");
         System.out.println(StringTasks.reverse(123));
         System.out.println("----------------------------------");
+        System.out.println("First uniquw character in a string");
+        System.out.println(StringTasks.firstUniqChar("leetcode"));
     }
 }
--- src/StringTasks.java
+++ src/StringTasks.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class StringTasks {
     /*
         Reverse string
@@ -56,6 +58,31 @@ public class StringTasks {
         }
         return true;
 
+    }
+
+    /*
+        First Unique Character in a String
+
+        Given a string s, find the first non-repeating character in
+        it and return its index. If it does not exist, return -1.
+     */
+
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), 2);
+            } else{
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
